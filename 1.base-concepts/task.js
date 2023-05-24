34ee283e8c390968b05a270d55ceef3d9cc5dd41
Anastasia Sokolova <sokolovaa922@gmail.com>
2023-05-24 23:37:26 +0500

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,21 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b**2 - 4 * a * c;
+  if (d < 0) {
+    return arr;
+  }
+  if (d === 0) {
+    let x = -b / (2 * a);
+    arr.push(x);
+    return arr;
+  }
+  let x1 = (-b + Math.sqrt(d) ) / (2 * a);
+  let x2 = (-b - Math.sqrt(d) ) / (2 * a);
+  arr.push(x1, x2)
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+
+} 
