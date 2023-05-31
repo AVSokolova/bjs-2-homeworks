cbfda9f603d358afdaecec1b104f7309fe036b58
Anastasia Sokolova <sokolovaa922@gmail.com>
2023-05-31 23:17:54 +0500

    Initial commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,21 @@
+"use strict";
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum = arr[i] + sum;
+  }
+
+  let avg = sum / arr.length;
+  avg = Math.round(avg * 100) / 100;
 
   return { min: min, max: max, avg: avg };
 }
