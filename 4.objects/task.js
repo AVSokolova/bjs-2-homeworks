80dffc6e0ec5d8919bf16eaf9ffd9113d5267e2e
Anastasia Sokolova <sokolovaa922@gmail.com>
2023-06-17 11:19:31 +0500

    added construtor function and setSubject method

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,13 +1,19 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = []
 }
 
+anna = new Student("Anna", "female", 20);
+john = new Student("John", "male", 21);
+
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+ 
 }
 
 Student.prototype.getAverage = function () {
