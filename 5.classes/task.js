3a531cc52e7c474586b5a53c01d0d2cd06342e5c
Anastasia Sokolova <sokolovaa922@gmail.com>
2023-06-27 12:57:31 +0500

    Created class PrintEditionItem, method set state and fix

---
+++ 5.classes/task.js
@@ -0,0 +1,27 @@
+class PrintEditionItem{
+    constructor(name, releaseDate, pagesCount, state = 100){
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this.state = state;
+        this.type = null;
+    }
+
+    set state(value) {
+        if (value < 0) {
+            this._state = 0;
+        }
+        else if (value > 100) {
+            this._state = 100;
+        }
+        else {
+            this._state = value;
+        }
+    }
+    
+    fix(){
+        this.state = this.state * 1.5;
+    }
+
+
+}
